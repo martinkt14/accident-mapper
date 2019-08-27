027780f631bd73d70e30403f4181667400b44b3b
Martin Teufel <teufel.m14@gmail.com>
2019-08-26 21:39:53 -0400

    Added clear map button

--- src/js/map.js
+++ src/js/map.js
@@ -5,28 +5,35 @@ let projectLocation;
 
 //Load Initial Map with Default Settings
 mapboxgl.accessToken =
-    "pk.eyJ1IjoibWFydGlua3QiLCJhIjoiY2p4Z3A2aDFxMDRzcjN4bXRqZmJ1ZWtnNCJ9.ADTijuOvLGo-XidvFsx7AA";
+  "pk.eyJ1IjoibWFydGlua3QiLCJhIjoiY2p4Z3A2aDFxMDRzcjN4bXRqZmJ1ZWtnNCJ9.ADTijuOvLGo-XidvFsx7AA";
 var map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/light-v10",
-    center: [-86.4, 39.8],
-    zoom: 6.5
+  container: "map",
+  style: "mapbox://styles/mapbox/light-v10",
+  center: [-86.4, 39.8],
+  zoom: 6.5
 });
+
+//Add Map Options
+var scale = new mapboxgl.ScaleControl({
+  maxWidth: 200,
+  unit: "imperial"
+});
+map.addControl(scale);
 
 //Plot Accident Data
 const mapData = accidents => {
-    accidents.forEach(accident => {
-        let popup = new mapboxgl.Popup().setHTML(
-            `<h1>Record Number: ${accident["Record Number"]}</h1>
-            <h2>Accident Type: Testing</h2>
+  accidents.forEach(accident => {
+    let popup = new mapboxgl.Popup().setHTML(
+      `<h1>Record Number: ${accident["Record Number"]}</h1>
+            <h2>Accident Type: ${accident["Accident Type"]}</h2>
             <p>${accident["Summary"]}</p>`
-        );
+    );
 
-        let marker = new mapboxgl.Marker()
-            .setLngLat([accident["Longitude"], accident["Latitude"]])
-            .setPopup(popup)
-            .addTo(map);
-    });
+    let marker = new mapboxgl.Marker()
+      .setLngLat([accident["Longitude"], accident["Latitude"]])
+      .setPopup(popup)
+      .addTo(map);
+  });
 };
 
 //Filter Accident Types
@@ -34,24 +41,24 @@ const mapData = accidents => {
 //Filter Accidents by Distance
 //Add Project Location Marker
 map.on("click", function(e) {
-    if (
-        document
-            .querySelector("#target-point-button")
-            .classList.contains("active")
-    ) {
-        const markerOptions = {
-            color: "red"
-        };
-        let marker = new mapboxgl.Marker(markerOptions)
-            .setLngLat(e.lngLat)
-            .addTo(map);
-        document
-            .querySelector("#target-point-button")
-            .classList.remove("active");
-        proxPickPoint.textContent = proxPickPoint.classList.contains("active")
-            ? "Cancel"
-            : "Pick Point";
-    }
+  if (
+    document
+      .querySelector("#target-point-button")
+      .classList.contains("btn-danger")
+  ) {
+    const markerOptions = {
+      color: "red"
+    };
+    let marker = new mapboxgl.Marker(markerOptions)
+      .setLngLat(e.lngLat)
+      .addTo(map);
+    document
+      .querySelector("#target-point-button")
+      .classList.remove("btn-danger");
+    proxPickPoint.textContent = proxPickPoint.classList.contains("btn-danger")
+      ? "Cancel"
+      : "Pick Point";
+  }
 });
 
 //Filter Accidents by Roadway
--- src/js/menus.js
+++ src/js/menus.js
@@ -1,120 +1,166 @@
+//Variables
+const overlay = document.querySelector(".overlay");
+//Navigation
 const navButtons = document.querySelectorAll(".nav-btn");
 const subMenus = document.querySelectorAll(".submenu");
 const closeButtons = document.querySelectorAll(".fa-times");
+//Map Style
 const mapStyleSelectors = document.querySelectorAll(".map-style-selector");
+//Accidents
 const accidentFileDrop = document.querySelector("#accident-file-drop");
 const dropLabel = document.querySelector("label[for=filedrop]");
 const fileSubmitButton = document.querySelector("#map-data");
+//Filters
 const proxPickPoint = document.querySelector("#target-point-button");
+const accidentModal = document.querySelector("#accident-upload-modal");
+const accidentStatMapped = document.querySelector("#accidents-mapped");
+const accidentStatErrors = document.querySelector("#accident-errors");
+const analyzeAccidents = document.querySelector(
+  "#accident-upload-modal > button"
+);
+const distanceSlider = document.querySelector("#distance-slider");
 
 ////////////////////////////////////////////////
 //Open Submenu(s)
 ////////////////////////////////////////////////
 navButtons.forEach(button => {
-    button.addEventListener("click", function() {
-        //Clear active button styling
-        navButtons.forEach(button => {
-            button.classList.remove("active");
-        });
-        //Close any/all submenus
-        subMenus.forEach(submenu => {
-            submenu.classList.remove("active");
-        });
-        //Add active styling to submenu button
-        button.classList.add("active");
-        //Open selected submenu by setting 'active' class
-        const menu = this.dataset.menu;
-        document.querySelector(menu).classList.add("active");
+  button.addEventListener("click", function() {
+    //Clear active button styling
+    navButtons.forEach(button => {
+      button.classList.remove("active");
     });
+    //Close any/all submenus
+    subMenus.forEach(submenu => {
+      submenu.classList.remove("active");
+    });
+    //Add active styling to submenu button
+    button.classList.add("active");
+    //Open selected submenu by setting 'active' class
+    const menu = this.dataset.menu;
+    document.querySelector(menu).classList.add("active");
+  });
 });
 
 ////////////////////////////////////////////////
 //Close Menu(s)
 ////////////////////////////////////////////////
 closeButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        subMenus.forEach(submenu => {
-            submenu.classList.remove("active");
-        });
-        navButtons.forEach(button => {
-            button.classList.remove("active");
-        });
+  button.addEventListener("click", () => {
+    subMenus.forEach(submenu => {
+      submenu.classList.remove("active");
     });
+    navButtons.forEach(button => {
+      button.classList.remove("active");
+    });
+  });
 });
 
 ////////////////////////////////////////////////
 //Change map style
 ///////////////////////////////////////////////
 mapStyleSelectors.forEach(selector => {
-    selector.addEventListener("click", function() {
-        //Set style variable from selected style button
-        const style = this.getAttribute("data-map-style");
-        //Set style on map
-        map.setStyle(style);
-        //Clear active styling from each style selector button
-        mapStyleSelectors.forEach(selector => {
-            selector.classList.remove("active");
-        });
-        //Add active styling to selected style button
-        selector.classList.add("active");
+  selector.addEventListener("click", function() {
+    //Set style variable from selected style button
+    const style = this.getAttribute("data-map-style");
+    //Set style on map
+    map.setStyle(style);
+    //Clear active styling from each style selector button
+    mapStyleSelectors.forEach(selector => {
+      selector.classList.remove("active");
     });
+    //Add active styling to selected style button
+    selector.classList.add("active");
+  });
 });
 
 ////////////////////////////////////////////////
 //Handle Accident File Drop/Selection
 ///////////////////////////////////////////////
 accidentFileDrop.addEventListener("dragover", function(event) {
-    event.preventDefault();
+  event.preventDefault();
 });
 
 accidentFileDrop.addEventListener("dragenter", function(event) {
-    event.preventDefault();
-    this.classList.add("active");
+  event.preventDefault();
+  this.classList.add("active");
 });
 
 accidentFileDrop.addEventListener("dragleave", function(event) {
-    event.preventDefault();
-    this.classList.remove("active");
+  event.preventDefault();
+  this.classList.remove("active");
 });
 
 accidentFileDrop.addEventListener("drop", function(event) {
-    event.preventDefault();
-    this.classList.remove("active");
+  event.preventDefault();
+  this.classList.remove("active");
 
-    dropLabel.textContent = event.dataTransfer.files[0].name;
-    document.querySelector("#filedrop").files = event.dataTransfer.files;
-    fileSubmitButton.classList.add("active");
+  dropLabel.textContent = event.dataTransfer.files[0].name;
+  document.querySelector("#filedrop").files = event.dataTransfer.files;
+  fileSubmitButton.classList.add("active");
 });
 
 document
-    .querySelector("input[id=filedrop]")
-    .addEventListener("change", function(event) {
-        // console.log(event.target.files[0]);
-        dropLabel.textContent = event.target.files[0].name;
-        fileSubmitButton.classList.add("active");
-    });
+  .querySelector("input[id=filedrop]")
+  .addEventListener("input", function(event) {
+    // console.log(event.target.files[0]);
+    dropLabel.textContent = event.target.files[0].name;
+    fileSubmitButton.classList.add("active");
+  });
 
 ////////////////////////////////////////////////
 //Handle Accident Map Data Button Press
 ///////////////////////////////////////////////
 accidentFileDrop.addEventListener("submit", function(e) {
-    e.preventDefault();
-    dropLabel.textContent = "Loading...";
-    //Set variable for form data
-    let formData = new FormData();
-    //Add file to formData variable
-    formData.append("filedrop", document.querySelector("#filedrop").files[0]);
-    //Send form data
-    fetch("../accidents", {
-        method: "post",
-        body: formData
-    }).then(async res => {
-        let accidentData = await res.json().then(data => {
-            return data;
-        });
-        mapData(accidentData);
-        dropLabel.textContent = "Data Successfully Uploaded!";
-    });
+  e.preventDefault();
+  dropLabel.textContent = "Loading...";
+  //Set variable for form data
+  let formData = new FormData();
+  //Add file to formData variable
+  formData.append("filedrop", document.querySelector("#filedrop").files[0]);
+  //Send form data
+  fetch("../accidents", {
+    method: "post",
+    body: formData
+  }).then(async res => {
+    try {
+      let accidentData = await res.json().then(data => {
+        return data;
+      });
+      mapData(accidentData);
+      dropLabel.textContent = "Data Successfully Uploaded!";
+      fileSubmitButton.classList.remove("active");
+      overlay.classList.add("active");
+      accidentModal.classList.add("active");
+    } catch (error) {
+      dropLabel.textContent = "Error: Unable to Load Data";
+    }
+  });
+});
+
+////////////////////////////////////////////////
+//Handle Accident Modal Analyze Data Button
+///////////////////////////////////////////////
+analyzeAccidents.addEventListener("click", () => {
+  accidentModal.classList.remove("active");
+  overlay.classList.remove("active");
+  clearMapButton.style = "display: block";
+});
+
+////////////////////////////////////////////////
+//Handle Clear Map Button
+///////////////////////////////////////////////
+const clearMapButton = document.querySelector("#clear-map-button");
+clearMapButton.addEventListener("click", () => {
+  //Set variable for all markers
+  let markers = document.querySelectorAll(".mapboxgl-marker");
+  //Clear/Remove all markers
+  markers.forEach(marker => marker.remove());
+  //Hide "Clear Map"
+  clearMapButton.style = "display: none";
+  //Reset text on file dropzone
+  dropLabel.innerHTML = `<strong>Chose a file</strong><span> or drag it here</span>`;
+  //Reset File Drop Input/Form
+  accidentFileDrop.reset();
 });
 
 ////////////////////////////////////////////////
@@ -122,12 +168,34 @@ accidentFileDrop.addEventListener("submit", function(e) {
 ///////////////////////////////////////////////
 
 //Proximity Filter
+//////////////////////////////////
 proxPickPoint.addEventListener("click", () => {
-    proxPickPoint.classList.toggle("active");
-    //Toggle Text
-    proxPickPoint.textContent = proxPickPoint.classList.contains("active")
-        ? "Cancel"
-        : "Pick Point";
+  proxPickPoint.classList.toggle("btn-danger");
+  //Toggle Text
+  proxPickPoint.textContent = proxPickPoint.classList.contains("btn-danger")
+    ? "Cancel"
+    : "Pick Point";
+});
+
+distanceSlider.addEventListener("input", function() {
+  const label = document.querySelector("label[for=slider]");
+  if (this.value == 0) {
+    return (label.textContent = "");
+  }
+  if (this.value <= 1) {
+    return (label.textContent = this.value + " mile");
+  }
+  return (label.textContent = this.value + " miles");
+});
+
+//Category Filter
+//////////////////////////////////
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
+
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener("click", function(e) {
+    console.log(this.getAttribute("data-checkbox-label"));
+  });
 });
 
 console.log("Menu JS loaded...");
